52891a2f545894fb0b21b714b4f9a3d13b9aff6b
lei <mia.wang90@gmail.com>
2022-04-14 22:48:06 +0900

    add index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @articles = Article.new
+    end
+
+    def create
+
+    end
+
+end
