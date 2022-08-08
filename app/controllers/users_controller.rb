62b4c27a232a153107d956c49e016a249b161882
lei <mia.wang90@gmail.com>
2022-08-08 22:19:00 +0900

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was updated successfully."
+      redirect_to articles_path
+    else
+      render :edit
+    end
   end
 
   private
