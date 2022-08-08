970c350087a11a99317506a7f9c0d9a60bedabb1
lei <mia.wang90@gmail.com>
2022-08-08 21:12:55 +0900

    Add one-to-many association between users and articles from back end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :articles
+
   validates :username, presence: true, 
                       uniqueness: true, 
                       length: { minimum: 3, maximum: 25 }
