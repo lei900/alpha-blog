f1435c8bbd5f5d9a71805e4904441b20e8253bca
lei <mia.wang90@gmail.com>
2022-08-08 20:58:38 +0900

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates :username, presence: true, 
+                      uniqueness: true, 
+                      length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address" }
 end
