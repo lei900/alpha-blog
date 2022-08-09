670d9eec678d1de2eff6b38a314a1f279665ae16
lei <mia.wang90@gmail.com>
2022-08-09 10:17:55 +0900

    Make sample articles use Faker

--- Gemfile
+++ Gemfile
@@ -28,10 +28,13 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'kaminari'
+
 group :development, :test do
   gem 'sqlite3', '~> 1.4'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create!(
+    username: Faker::Book.author,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+100.times do |article|
+  Article.create(
+      user: User.offset(rand(User.count)).first,
+      title: Faker::Book.title,
+      description: Faker::Quote.matz
+  )
+end
