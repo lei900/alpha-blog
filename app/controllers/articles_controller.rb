0ab6063695cc8318f691e22e80d292bcc3eaef69
lei <mia.wang90@gmail.com>
2022-08-09 10:44:39 +0900

    Add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
     def show; end
 
     def index
-        @articles = Article.all.order(created_at: :desc)
+        @articles = Article.all.includes(:user).order(created_at: :desc).page(params[:page])
     end
 
     def new
